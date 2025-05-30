37caef74e16412d990c3e8112a4f145360d91d38
daniilstudenikin <d.studenikin@action.tech>
2025-05-30 18:06:52 +0300

    Initialize Django project structure with ASGI, WSGI, settings, and basic app configuration

---
+++ backend/foodgram/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from django.urls.conf import re_path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    re_path(r'^auth/', include('djoser.urls')),
+    re_path(r'^auth/', include('djoser.urls.jwt')),
+]
