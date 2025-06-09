6684fb659556d64f960d49d8236a00e5e69dda3f
daniilstudenikin <d.studenikin@action.tech>
2025-06-09 10:17:40 +0300

    refactor: Add default ordering to Subscription model by creation date

--- backend/users/models.py
+++ backend/users/models.py
@@ -49,6 +49,7 @@ class Subscription(models.Model):
     class Meta:
         verbose_name = 'Подписка'
         verbose_name_plural = 'Подписки'
+        ordering = ('-created',)
         constraints = [
             models.UniqueConstraint(
                 fields=['user', 'author'],
